c2b1102cc78ce3274b06d1f4625cfe4bb78c7515
JoseN2 <87278438+JoseN2@users.noreply.github.com>
2024-10-30 21:50:43 -0600

    Add files via upload

---
+++ Pratice.ps1
@@ -0,0 +1,9 @@
+﻿#gather user input and store it 2 lines 
+$input = Read-Host -prompt "Please enter your name"
+ #Write-Host "Hello $input ,it's nice to meet you"
+    if ($input -eq "Kevin") {
+        Write-Host "Hello Kevin what are you doing on my PC"
+   } elseif ($input -eq "Jose") {
+            Write-Host "Hello Master Welcome back home $input"
+    }else { "Hello $input who are you"
+ }
